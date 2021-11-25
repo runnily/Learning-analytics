c8cdf2d7883a39ea2880d81abf5bf4a7570971cd
runnily <32201807+runnily@users.noreply.github.com>
2021-11-25 15:59:43 +0000

    Add 3 helper functions for preprocessing the data including plots in report used for analysis; some data files needed to be readded

--- lib/helpers.R
+++ lib/helpers.R
@@ -1,4 +1,34 @@
-helper.function <- function()
-{
-  return(1)
+numberOfTypes = function(df, type){
+  #' Count the number of distinct of a specified
+  #' type withing a data frame
+  #'
+  #' @description from a given data frame and specified
+  #'              type it calculates the number of distinct
+  #'              types within the data frame
+  #' @param df  data frame name
+  #' @param type The type to count 
+  #' @return A tibble illustrating the number of distinct types
+  df = df %>% filter({{type}} != c("Unknown"))
+  return ((df %>% group_by({{type}})) %>% tally())
+}
+
+typeOfUser = function(df, id, type) {
+  #' This is used to find the type of user for a particular
+  #' Data frame
+  #'
+  #' @description This function will get the type of user
+  #'              from a specific data frame
+  #' @param df  data frame name
+  #' @param type The type to count 
+  #' @return A tibble illustrating the number of distinct types
+  return(df[[type]][df$learner_id == id])
+}
+
+translateCodeToCnt = function(srows) {
+  #'This takes a specific data frame and translates
+  #'them applies a region to it.
+  #'
+  map = data.frame(row.names = code$alpha.2, val = code$region)
+  srows$region = map[srows$detected_country,]
+  return(srows)
 }
--- munge/01-A.R
+++ munge/01-A.R
@@ -1,1 +1,5 @@
 # Example preprocessing script.
+total_enrollments = translateCodeToCnt(distinct(rbind(cyber.security.1_enrolments, cyber.security.2_enrolments, 
+                          cyber.security.3_enrolments, cyber.security.4_enrolments, 
+                          cyber.security.5_enrolments, cyber.security.6_enrolments,
+                          cyber.security.7_enrolments), learner_id, .keep_all = TRUE))
