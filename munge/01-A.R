c6a9fa2326f20b0519de9676a59589e1006c6b13
runnily <32201807+runnily@users.noreply.github.com>
2021-11-30 14:48:51 +0000

    Added some analysis to report including redefining the data used for the predictive model

--- munge/01-A.R
+++ munge/01-A.R
@@ -20,11 +20,24 @@ total_vids_stats = totalVidStats(list(translateVideoStats(cyber.security.3_video
 
 total_enrollements_model_data = total_enrollments[-c(2:6,13,8)]
 total_enrollements_model_data = total_enrollements_model_data %>% filter_all(all_vars(.!="Unknown"))
+
 total_enrollements_model_data = na.omit(performanceVsDf(total_enrollements_model_data))
+
+avg_quizes_weekly = avgNumberOfQuizCompletedWeekly(total_quizes)
+avg_quizes_weekly = data.frame(row.names = avg_quizes_weekly$learner_id, vals = avg_quizes_weekly$mean_num_quizes_weekly)
+total_enrollements_model_data$mean_num_quizes_weekly = avg_quizes_weekly[total_enrollements_model_data$learner_id,]
+
+avg_activties_weekly = avgNumberOfActivtiesCompletedWeekly(total_activties)
+avg_activties_weekly = data.frame(row.names = avg_activties_weekly$learner_id, vals = avg_activties_weekly$mean_num_activties_weekly)
+total_enrollements_model_data$mean_num_activties_weekly = avg_activties_weekly[total_enrollements_model_data$learner_id,]
+
 total_enrollements_model_data = total_enrollements_model_data[-1]
 performance = total_enrollements_model_data$performance
-total_enrollements_model_data = apply(total_enrollements_model_data[-7], 2, asDoubleFactor)
-total_enrollements_model_data = data.frame(total_enrollements_model_data, performance)
+avg_quizes_weekly = total_enrollements_model_data$mean_num_quizes_weekly
+avg_activties_weekly = total_enrollements_model_data$mean_num_activties_weekly
+  
+total_enrollements_model_data = apply(total_enrollements_model_data[-c(7:9)], 2, asDoubleFactor)
 
-
+total_enrollements_model_data = data.frame(total_enrollements_model_data, avg_quizes_weekly, avg_activties_weekly, performance)
+total_enrollements_model_data = na.omit(total_enrollements_model_data)
 
